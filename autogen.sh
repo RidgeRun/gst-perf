7cd75e8c9404787736ef2207fdee22b0d89d2822
Michael Gruner <michael.gruner@ridgerun.com>
2019-07-09 14:56:31 -0600

    Modify autogen.sh to allow cross-compilation

--- autogen.sh
+++ autogen.sh
@@ -3,16 +3,17 @@
 # ACLOCAL, AUTOPOINT and/or LIBTOOLIZE to the right versions, or leave them
 # unset and get the defaults
 
-autoreconf --verbose --force --install --make || {
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
+
+olddir=`pwd`
+cd "$srcdir"
+
+autoreconf --verbose --force --install || {
  echo 'autogen.sh failed';
  exit 1;
 }
 
-./configure || {
- echo 'configure failed';
- exit 1;
-}
-
 echo
-echo "Now type 'make' to compile this module."
+echo "Now you can proceed to configure this project"
 echo
